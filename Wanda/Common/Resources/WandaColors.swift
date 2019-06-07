f87f3ec153a3ba717add1fb5307f07cb1c578a3f
Courtney Bell <bellcourtney@yahoo.com>
2019-06-06 23:28:49 -0700

    Add Wanda Colors helper class that contains colors by hex string based on color name.

---
+++ Wanda/Common/Resources/WandaColors.swift
@@ -0,0 +1,21 @@
+//
+//  WandaColors.swift
+//  Wanda
+//
+//  Created by Bell, Courtney on 2/22/19.
+//  Copyright © 2019 Bell, Courtney. All rights reserved.
+//
+
+import UIKit
+
+struct WandaColors {
+    static let errorRed = UIColor(hexString: "#FA3F47")
+    static let lightGrey = UIColor(hexString: "#FAFAFA")
+    static let lightPurple = UIColor(hexString: "#986699")
+    static let limeGreen = UIColor(hexString: "#99CC34")
+    static let mutedBlack = UIColor(hexString: "#021829")
+    static let offWhite = UIColor(hexString: "#FEFFFE")
+    static let mediumGrey = UIColor(hexString: "#8B8B8B")
+    static let darkPurple = UIColor(hexString: "#663498")
+    static let newErrorRed = UIColor(hexString: "#FF6E6E")
+}
