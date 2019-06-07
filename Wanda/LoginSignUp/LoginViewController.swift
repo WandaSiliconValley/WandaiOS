81c4aa54bcecc3d0ec1ccd867a218491a425777d
Courtney Bell <bellcourtney@yahoo.com>
2019-06-06 23:16:41 -0700

    Create Login and Sign Up view controllers.

---
+++ Wanda/LoginSignUp/LoginViewController.swift
@@ -0,0 +1,294 @@
+//
+//  LoginViewController.swift
+//  Wanda
+//
+//  Created by Bell, Courtney on 11/19/18.
+//  Copyright © 2018 Bell, Courtney. All rights reserved.
+//
+
+import EventKit
+import EventKitUI
+import Firebase
+import FirebaseAuth
+import MessageUI
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate, MFMailComposeViewControllerDelegate, WandaAlertViewDelegate, EKEventEditViewDelegate {
+    @IBOutlet private weak var emailInfoLabel: UILabel!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var passwordInfoLabel: UILabel!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var showHidePasswordButton: UIButton!
+    @IBOutlet private weak var spinner: UIActivityIndicatorView!
+    
+    private var actionState: ActionState = .contactUs
+    private var dataManager = WandaDataManager.shared
+    private var firebaseId: String?
+    private var isValidEmail = false
+    private var isValidPassword = false
+    private var showHideIconClicked = false
+    
+    private enum ActionState {
+        case contactUs
+        case retryGetClasses
+        case retryGetMother
+        case retryLogin
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            navigationBar.barTintColor = WandaColors.darkPurple
+            navigationBar.isTranslucent = true
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // to do look at ease scene styles
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationBar.shadowImage = UIImage()
+            navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 80.0)
+        }
+        passwordTextField.underlined()
+        emailTextField.underlined()
+    }
+    
+    override func viewDidLoad() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    // MARK: Private
+    
+    @objc
+    private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        var userInfo = notification.userInfo!
+        let keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 60
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc
+    private func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+        
+        if let nextResponder = textField.superview?.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    private func loginWithFirebase(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            guard let firebaseId = authResult?.user.uid else {
+                if let error = error, let errorCode = AuthErrorCode(rawValue: error._code) {
+                    self.spinner.toggleSpinner(for: self.loginButton, title: LoginSignUpStrings.login)
+                    let errorString = FirebaseError(errorCode: errorCode.rawValue)
+                    self.logAnalytic(tag: "\(WandaAnalytics.loginError)_\(errorString.rawValue)")
+                    switch errorCode {
+                        case .networkError:
+                            // Set action state to retry login so the user has the option to retry the API call.
+                            self.actionState = .retryLogin
+                            self.presentErrorAlert(for: .networkError)
+                        case .invalidEmail, .missingEmail:
+                            _ = email.isEmailValid(emailTextField: self.emailTextField, emailInfoLabel: self.emailInfoLabel)
+                        case .wrongPassword, .userNotFound:
+                            self.emailInfoLabel.configureError(ErrorStrings.invalidCredentials, invalidTextField: self.emailTextField)
+                        default:
+                            // Set action state to retry login so the user has the option to retry the API call.
+                            self.actionState = .retryLogin
+                            self.presentErrorAlert(for: .systemError)
+                    }
+                }
+                
+                return
+            }
+            
+            // Ensure action state is set back to contact us if the call was successful.
+            self.actionState = .contactUs
+            // We store the firebase id for the case that the get mother api call needs to be retried.
+            self.firebaseId = firebaseId
+            self.getWandaMother()
+        }
+    }
+    
+    private func getWandaMother() {
+        guard let firebaseId = firebaseId else {
+            return
+        }
+
+        dataManager.getWandaMother(firebaseId: firebaseId) { success, error in
+            guard success else {
+                if let error = error {
+                    // Set action state to retry get mother so the user has the option to retry the API call.
+                    self.actionState = .retryGetMother
+                    self.spinner.toggleSpinner(for: self.loginButton, title: LoginSignUpStrings.login)
+                    switch error {
+                        case .networkError:
+                            self.presentErrorAlert(for: .networkError)
+                        case .unknown:
+                            self.presentErrorAlert(for: .systemError)
+                    }
+                }
+                
+                return
+            }
+
+            // Ensure action state is set back to contact us if the call was successful.
+            self.actionState = .contactUs
+            self.getClasses()
+        }
+    }
+    
+    private func getClasses() {
+        self.dataManager.getWandaClasses() { success, error in
+            guard success else {
+                
+                if let error = error {
+                    // Set action state to retry get classes so the user has the option to retry the API call.
+                    self.actionState = .retryGetClasses
+                    self.spinner.toggleSpinner(for: self.loginButton, title: LoginSignUpStrings.login)
+                    switch error {
+                        case .networkError:
+                            self.presentErrorAlert(for: .networkError)
+                        case .unknown:
+                            self.presentErrorAlert(for: .systemError)
+                    }
+                }
+                return
+            }
+            
+            // Ensure action state is set back to contact us if the call was successful.
+            self.actionState = .contactUs
+            DispatchQueue.main.async {
+                guard let classesViewController = ViewControllerFactory.makeClassesViewController() else {
+                    assertionFailure("Could not instantiate ClassesViewController.")
+                    return
+                }
+                
+                self.spinner.toggleSpinner(for: self.loginButton, title: LoginSignUpStrings.login)
+                self.dataManager.needsReload = true
+                self.navigationController?.pushViewController(classesViewController, animated: true)
+            }
+        }
+    }
+    
+    // to do how is this being used here right now?
+    private func contactUs() {
+        guard let contactUsViewController = ViewControllerFactory.makeContactUsViewController(for: .login) else {
+            self.presentErrorAlert(for: .contactUsError)
+            return
+        }
+        
+        contactUsViewController.mailComposeDelegate = self
+        self.present(contactUsViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction private func didEditEmail() {
+        emailInfoLabel.configureValidEmail(emailTextField)
+    }
+    
+    @IBAction private func didEditPassword() {
+        passwordInfoLabel.isHidden = true
+        passwordTextField.underlined()
+    }
+    
+    @IBAction private func didTapShowHideButton() {
+        let icon = showHideIconClicked ? WandaImages.eyeIcon : WandaImages.eyeOffIcon
+        showHidePasswordButton.setImage(icon, for: .normal)
+        passwordTextField.isSecureTextEntry = showHideIconClicked
+        showHideIconClicked = !showHideIconClicked
+    }
+    
+    @IBAction private func didTapForgotPassword() {
+        guard let forgotPasswordViewController = ViewControllerFactory.makeForgotPasswordController() else {
+            assertionFailure("Could not load the ForgotPasswordViewController.")
+            return
+        }
+        
+        if let email = emailTextField.text {
+            forgotPasswordViewController.email = email
+        }
+        
+        logAnalytic(tag: WandaAnalytics.loginForgotPasswordTapped)
+        self.navigationController?.pushViewController(forgotPasswordViewController, animated: true)
+    }
+    
+    @IBAction func didTapSignUp() {
+        guard let signUpViewController = ViewControllerFactory.makeSignUpViewController() else {
+            assertionFailure("Could not load the SignUpViewController.")
+            return
+        }
+        
+        logAnalytic(tag: WandaAnalytics.loginSignUpButtonTapped)
+        self.navigationController?.pushViewController(signUpViewController, animated: true)
+    }
+    
+    private func verifySignUp() -> Bool {
+        guard let email = emailTextField.text, let password = passwordTextField.text else {
+            return false
+        }
+        
+        let emailValid = email.isEmailValid(emailTextField: emailTextField, emailInfoLabel: emailInfoLabel)
+        let passwordValid = password.isPasswordValid(passwordTextField: passwordTextField, passwordInfoLabel: passwordInfoLabel, checkLength: false)
+        
+        return emailValid && passwordValid
+    }
+    
+    @IBAction private func didTapLogin() {
+        guard verifySignUp(), let email = emailTextField.text, let password = passwordTextField.text else {
+            return
+        }
+        
+        
+        logAnalytic(tag: WandaAnalytics.loginButtonTapped)
+        spinner.toggleSpinner(for: loginButton, title: LoginSignUpStrings.login)
+        loginWithFirebase(email: email, password: password)
+    }
+    
+    // MARK: WandaAlertViewDelegate
+    
+    func didTapActionButton() {
+        switch actionState {
+            case .contactUs:
+                contactUs()
+            case .retryGetClasses:
+                getClasses()
+            case .retryGetMother:
+                getWandaMother()
+            case .retryLogin:
+                didTapLogin()
+        }
+    }
+    
+    // MARK: EKEventEditViewDelegate
+    
+    func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith action: EKEventEditViewAction) {
+        controller.dismiss(animated: true)
+    }
+}
---
+++ Wanda/LoginSignUp/SignUpViewController.swift
@@ -0,0 +1,292 @@
+//
+//  SignUpViewController.swift
+//  Wanda
+//
+//  Created by Bell, Courtney on 1/4/19.
+//  Copyright © 2019 Bell, Courtney. All rights reserved.
+//
+
+import Firebase
+import MessageUI
+import UIKit
+
+class SignUpViewController: UIViewController, UITextFieldDelegate, MFMailComposeViewControllerDelegate, WandaAlertViewDelegate {
+    @IBOutlet private weak var confirmPasswordInfoLabel: UILabel!
+    @IBOutlet private var confirmPasswordTextField: UITextField!
+    @IBOutlet private weak var emailInfoLabel: UILabel!
+    @IBOutlet private var emailTextField: UITextField!
+    @IBOutlet private weak var passwordInfoLabel: UILabel!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var showHidePasswordButton: UIButton!
+    @IBOutlet private weak var showHideConfirmPasswordButton: UIButton!
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var spinner: UIActivityIndicatorView!
+    
+    private var actionState: ActionState = .retryCreateFirebaseUser
+    private var dataManager = WandaDataManager.shared
+    private var motherId: String?
+    private var password: String {
+        guard let password = passwordTextField.text else {
+            return ""
+        }
+        
+        return password.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    private var confirmationPassword: String {
+        guard let confirmationPassword = confirmPasswordTextField.text else {
+            return ""
+        }
+        
+        return confirmationPassword.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    private var passwordsMatch: Bool {
+        return password.trimmingCharacters(in: .whitespacesAndNewlines) == confirmationPassword.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    private var showPasswordClicked = false
+    private var showConfirmPasswordClicked = false
+    
+    private enum ActionState {
+        case retryCreateFirebaseUser
+        case retryCreateWandaMother
+    }
+    
+    static let storyboardIdentifier = String(describing: SignUpViewController.self)
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let navigationBar = self.navigationController?.navigationBar {
+            navigationBar.isTranslucent = true
+            navigationBar.setBackgroundImage(UIImage(), for: .default)
+            navigationBar.shadowImage = UIImage()
+            navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 80.0)
+        }
+    }
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        configureNavigationBar()
+        // to do !!
+        //        passwordTextField.isSecureTextEntry = true
+        //        confirmPasswordTextField.isSecureTextEntry = true
+        self.view.layoutIfNeeded()
+        passwordTextField.underlined()
+        emailTextField.underlined()
+        confirmPasswordTextField.underlined()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    // MARK: Private
+    
+    @objc
+    private func keyboardWillShow(notification:NSNotification){
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        let keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        var contentInset:UIEdgeInsets = self.scrollView.contentInset
+        contentInset.bottom = keyboardFrame.size.height + 60
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc
+    private func keyboardWillHide(notification:NSNotification){
+        let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInset
+    }
+    
+    @objc
+    private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc
+    private func backButtonPressed() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: WandaImages.backArrow, style: .plain, target: self, action: #selector(backButtonPressed))
+        
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.wandaFontBold(size: 20)]
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+        
+        if let nextResponder = textField.superview?.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    private func verifySignUp() -> Bool {
+        guard let email = emailTextField.text else {
+            return false
+        }
+        
+        if !passwordsMatch {
+            passwordInfoLabel.configureError(LoginSignUpStrings.passwordsDoNotMatch, invalidTextField: passwordTextField)
+        }
+        
+        let emailValid = email.isEmailValid(emailTextField: emailTextField, emailInfoLabel: emailInfoLabel)
+        let passwordValid = password.isPasswordValid(passwordTextField: passwordTextField, passwordInfoLabel: passwordInfoLabel, checkLength: true)
+        let confirmationPasswordValid = isConfirmationPasswordValid()
+        
+        return emailValid && passwordValid && confirmationPasswordValid && passwordsMatch
+    }
+    
+    private func isConfirmationPasswordValid() -> Bool {
+        if password.isEmpty && !confirmationPassword.isEmpty {
+            passwordInfoLabel.configureError(LoginSignUpStrings.passwordsDoNotMatch, invalidTextField: passwordTextField)
+            return false
+        }
+        
+        return true
+    }
+    
+    private func contactSupport() {
+        guard let contactUsViewController = ViewControllerFactory.makeContactUsViewController(for: .signUp) else {
+            self.presentErrorAlert(for: .contactUsError)
+            return
+        }
+        
+        contactUsViewController.mailComposeDelegate = self
+        self.present(contactUsViewController, animated: true, completion: nil)
+    }
+    
+    // MARK: IBActions
+    
+    @IBAction private func didEditEmail() {
+        emailInfoLabel.configureValidEmail(emailTextField)
+    }
+    
+    @IBAction private func didEditPassword() {
+        passwordInfoLabel.isHidden = true
+        passwordTextField.underlined()
+        confirmPasswordInfoLabel.isHidden = true
+    }
+    
+    @IBAction private func didTapShowHidePasswordButton() {
+        let icon = showPasswordClicked ? WandaImages.eyeIcon : WandaImages.eyeOffIcon
+        showHidePasswordButton.setImage(icon, for: .normal)
+        passwordTextField.isSecureTextEntry = showPasswordClicked
+        showPasswordClicked = !showPasswordClicked
+    }
+    
+    @IBAction private func didTapShowHideConfirmPasswordButton() {
+        let icon = showConfirmPasswordClicked ? WandaImages.eyeIcon : WandaImages.eyeOffIcon
+        showHidePasswordButton.setImage(icon, for: .normal)
+        showHideConfirmPasswordButton.imageView?.image  = showConfirmPasswordClicked ? WandaImages.eyeOffIcon : WandaImages.eyeIcon
+        confirmPasswordTextField.isSecureTextEntry = showConfirmPasswordClicked
+        showConfirmPasswordClicked = !showConfirmPasswordClicked
+    }
+    
+    @IBAction private func didTapSignUp() {
+        guard verifySignUp(), let email = emailTextField.text else {
+            return
+        }
+        
+        spinner.toggleSpinner(for: signUpButton, title: GeneralStrings.submitAction)
+        logAnalytic(tag: WandaAnalytics.signUpButtonTapped)
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            guard let motherId = authResult?.user.uid else {
+                if let error = error, let errorCode = AuthErrorCode(rawValue: error._code) {
+                    self.spinner.toggleSpinner(for: self.signUpButton, title: GeneralStrings.submitAction)
+                    let errorString = FirebaseError(errorCode: errorCode.rawValue)
+                    self.logAnalytic(tag: "\(WandaAnalytics.signUpError)_\(errorString.rawValue)")
+                    switch errorCode {
+                        case .networkError:
+                            self.actionState = .retryCreateFirebaseUser
+                            self.presentErrorAlert(for: .networkError)
+                        case .invalidEmail, .missingEmail:
+                            _ = email.isEmailValid(emailTextField: self.emailTextField, emailInfoLabel: self.emailInfoLabel)
+                        case .emailAlreadyInUse:
+                            self.emailInfoLabel.configureError(LoginSignUpStrings.emailInUse, invalidTextField: self.emailTextField)
+                        case .wrongPassword, .userNotFound:
+                            self.confirmPasswordInfoLabel.text = ErrorStrings.invalidCredentials
+                            self.confirmPasswordInfoLabel.isHidden = false
+                        default:
+                            self.actionState = .retryCreateFirebaseUser
+                            self.presentErrorAlert(for: .systemError)
+                    }
+                }
+                
+                return
+            }
+            
+            self.motherId = motherId
+            self.confirmPasswordInfoLabel.isHidden = true
+            self.createWandaMother()
+        }
+    }
+    
+    private func createWandaMother() {
+        guard verifySignUp(), let email = emailTextField.text, let motherId = motherId else {
+            return
+        }
+        
+        self.dataManager.createWandaAccount(firebaseId: motherId, email: email) { success, error in
+            guard success, let signUpSuccessViewController = ViewControllerFactory.makeWandaSuccessController() else {
+                Auth.auth().currentUser?.delete { error in
+                    // to do should we fail this siletnly and just log an analytics tag?
+                    print("Couldn't delete user")
+                }
+                
+                if let error = error {
+                    self.actionState = .retryCreateWandaMother
+                    self.spinner.toggleSpinner(for: self.signUpButton, title: GeneralStrings.submitAction)
+                    switch error {
+                        case .networkError:
+                            self.presentErrorAlert(for: .networkError)
+                        case .unknown:
+                            self.presentErrorAlert(for: .systemError)
+                    }
+                }
+                
+                return
+            }
+            signUpSuccessViewController.successType = .signUp
+            self.spinner.toggleSpinner(for: self.signUpButton, title: GeneralStrings.submitAction)
+            self.navigationController?.pushViewController(signUpSuccessViewController, animated: true)
+        }
+    }
+    
+    @IBAction private func didTapContactUs() {
+        // to do we will want contact support once we get the retry limit in place - retry 2 times then contact support
+        logAnalytic(tag: WandaAnalytics.signUpContactUsTapped)
+        contactSupport()
+    }
+    
+    // MARK: WandaAlertViewDelegate
+    
+    func didTapActionButton() {
+        switch actionState {
+            case .retryCreateFirebaseUser:
+                didTapSignUp()
+            case .retryCreateWandaMother:
+                createWandaMother()
+        }
+    }
+
+    // MARK: MFMailComposeViewControllerDelegate
+
+    func mailComposeController(_ controller: MFMailComposeViewController,
+                               didFinishWith result: MFMailComposeResult, error: Error?) {
+        // Check the result or perform other tasks.
+
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
