0fd9dac8516cbbca036eb5c814f196c2838ed7c3
Courtney Bell <bellcourtney@yahoo.com>
2019-06-06 23:27:35 -0700

    Add UIViewController extension that can present an error alert, log an analytic tag, or pop back to a view controller of a specified type if it exists in the stack.

---
+++ Wanda/Common/Extensions/UIViewController+Wanda.swift
@@ -0,0 +1,44 @@
+//
+//  UIViewController+Wanda.swift
+//  Wanda
+//
+//  Created by Courtney & Matt on 4/4/19.
+//  Copyright © 2019 Bell, Courtney. All rights reserved.
+//
+
+import FirebaseAnalytics
+import UIKit
+
+extension UIViewController {
+    func presentErrorAlert(for type: WandaAlertType) {
+        guard let selfAsDelegate = self as? WandaAlertViewDelegate, let wandaAlertViewController = ViewControllerFactory.makeWandaAlertController(type, delegate: selfAsDelegate) else {
+            return
+        }
+
+        self.present(wandaAlertViewController, animated: true) {
+            wandaAlertViewController.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissAlert)))
+        }
+    }
+    
+    func popBack<T: UIViewController>(toControllerType: T.Type) {
+        if var viewControllers: [UIViewController] = self.navigationController?.viewControllers {
+            viewControllers = viewControllers.reversed()
+            for currentViewController in viewControllers {
+                if currentViewController .isKind(of: toControllerType) {
+                    self.navigationController?.popToViewController(currentViewController, animated: true)
+                    break
+                }
+            }
+        }
+    }
+    
+    // to do make this strict to the analytics tag strings - enum?
+    func logAnalytic(tag: String) {
+        Analytics.logEvent(tag, parameters: nil)
+    }
+    
+    @objc
+    private func dismissAlert() {
+        dismiss(animated: true, completion: nil)
+    }
+}
