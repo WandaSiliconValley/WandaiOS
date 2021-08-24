d9d77146551b69e47ed4835b9fb859b3306bb1ab
Courtney Bell <>
2021-08-23 23:56:26 -0700

    Update to return true

--- Wanda/Common/Extensions/UIViewController+Wanda.swift
+++ Wanda/Common/Extensions/UIViewController+Wanda.swift
@@ -29,7 +29,7 @@ extension UIViewController {
                 if currentViewController .isKind(of: toControllerType) {
                     self.dismiss(animated: false)
                     self.navigationController?.popToViewController(currentViewController, animated: true)
-                    break
+                    return true
                 }
             }
         }
